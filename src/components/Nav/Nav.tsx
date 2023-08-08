529ce18908159156b1d6d76cfa5bb8469ec107dd
Roman Silivestrov <roman.silivestrov@metalamp.io>
2023-08-08 16:36:41 +0300

    Add husky, lint-staged

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,8 +1,8 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export const Nav: React.FC = () => (
   <nav>
-    <Link href='/'>Home</Link>
-    <Link href='/about'>About</Link>
+    <Link href="/">Home</Link>
+    <Link href="/about">About</Link>
   </nav>
-)
+);
